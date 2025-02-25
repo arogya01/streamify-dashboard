448e4f76c09a7c5387ddd22c2de10e066af8c641
Arogya Bichpuria <arogya.bichpuria@M-FVFHN7U3Q05N.local>
2025-02-25 23:08:06 +0530

    removed unusable comps

---
+++ mockData.ts
@@ -0,0 +1,24 @@
+export const mockData = {
+    userGrowth: [
+        { month: "Jan", totalUsers: 400000, activeUsers: 300000 },
+        { month: "Feb", totalUsers: 500000, activeUsers: 350000 },
+        { month: "Mar", totalUsers: 600000, activeUsers: 400000 },
+        { month: "Apr", totalUsers: 700000, activeUsers: 500000 },
+        { month: "May", totalUsers: 800000, activeUsers: 600000 },
+        { month: "Jun", totalUsers: 900000, activeUsers: 700000 },
+        { month: "Jul", totalUsers: 950000, activeUsers: 750000 },
+        { month: "Aug", totalUsers: 1000000, activeUsers: 800000 },
+        { month: "Sep", totalUsers: 1050000, activeUsers: 850000 },
+        { month: "Oct", totalUsers: 1100000, activeUsers: 900000 },
+        { month: "Nov", totalUsers: 1150000, activeUsers: 950000 },
+        { month: "Dec", totalUsers: 1200000, activeUsers: 1000000 },
+      ], 
+      topSongs: [
+        { name: "Song 1", streams: 100000 },
+        { name: "Song 2", streams: 80000 },
+        { name: "Song 3", streams: 70000 },
+        { name: "Song 4", streams: 60000 },
+        { name: "Song 5", streams: 50000 },
+      ]
+      
+}
